07bdabc10a9583e4d79b7305756d09a358df0a7a
CaseyMartin23 <cm232311@gmail.com>
2020-08-10 23:34:19 +0200

    Working Back-end and Front-end

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "client-side/build")));
 
+app.get("/getList", (req, res) => {
+  var list = ["item1", "item2", "item3"];
+  res.json(list);
+  console.log("Sent list of items");
+});
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client-side/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client-side/build/index.html"));
 });
 
-app.get("/test", (req, res) => {
-  res.send(JSON.parse({ test: "TEST is working!!!" }));
-});
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`server is listening on port ${port}...`);
+  console.log("App is listening on port " + port);
 });
