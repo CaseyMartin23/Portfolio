e7b6d11a7aff36136d67ec682c5a861a304077a5
CaseyMartin23 <cm232311@gmail.com>
2020-08-12 20:20:52 +0200

    Router Bug

--- back-end/server.js
+++ back-end/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const emailer = require("./emailer.js");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "front-end/build")));
+app.use(express.static(path.join(__dirname, "../front-end/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(emailer);
